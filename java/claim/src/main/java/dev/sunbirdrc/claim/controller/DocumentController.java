034ef2288f3cb9eba0f45558e345f423b24d742d
Pawan k <128481755+kumarpawantarento@users.noreply.github.com>
2023-09-07 15:46:47 +0530

    digilocker uri store

---
+++ java/claim/src/main/java/dev/sunbirdrc/claim/controller/DocumentController.java
@@ -0,0 +1,37 @@
+
+package dev.sunbirdrc.claim.controller;
+
+import dev.sunbirdrc.claim.entity.Document;
+import dev.sunbirdrc.claim.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/digilicker")
+public class DocumentController {
+    private final DocumentService documentService;
+
+    @Autowired
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Document> saveDocument(@RequestBody Document document) {
+        Document savedDocument = documentService.saveDocument(document);
+        return new ResponseEntity<>(savedDocument, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/osid/{osid}")
+    public ResponseEntity<Document> getDocumentByOsid(@PathVariable String osid) {
+        Document document = documentService.getDocumentByOsid(osid);
+        if (document != null) {
+            return new ResponseEntity<>(document, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
+
---
+++ java/claim/src/main/java/dev/sunbirdrc/claim/service/DocumentService.java
@@ -0,0 +1,25 @@
+package dev.sunbirdrc.claim.service;
+
+import dev.sunbirdrc.claim.entity.Document;
+import dev.sunbirdrc.claim.repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentService {
+    private DocumentRepository documentRepository;
+
+    @Autowired
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    public Document saveDocument(Document document) {
+        return documentRepository.save(document);
+    }
+
+    public Document getDocumentByOsid(String osid) {
+        return documentRepository.findByOsid(osid);
+    }
+
+}
