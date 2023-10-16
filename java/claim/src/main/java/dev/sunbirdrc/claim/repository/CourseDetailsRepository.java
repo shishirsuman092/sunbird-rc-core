caf35c893db5a001fa53ff836a19c8fb871c226a
rkrahu <rajendra.kishor@tarento.com>
2023-10-16 13:35:18 +0530

    CourseDetails addition

---
+++ java/claim/src/main/java/dev/sunbirdrc/claim/repository/CourseDetailsRepository.java
@@ -0,0 +1,21 @@
+package dev.sunbirdrc.claim.repository;
+
+import dev.sunbirdrc.claim.entity.CourseDetails;
+import dev.sunbirdrc.claim.entity.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CourseDetailsRepository extends JpaRepository<CourseDetails, Long> {
+    List<CourseDetails> findByCouncilNameAndCourseNameAndActivityName(String councilName, String courseName, String activityName);
+
+    @Query("SELECT cd.courseName FROM CourseDetails cd WHERE cd.councilName = :courseName and cd.courseName = :courseName " +
+            "and cd.activityName = :activityName and cd.isGoodStanding = :isGoodStanding and cd.isForeignVerification = :isForeignVerification")
+    List<CourseDetails> findCourseKey(String councilName, String courseName, String activityName, Boolean isGoodStanding, Boolean isForeignVerification);
+}
+
